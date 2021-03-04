fdfb2434be3f8f6ca2e9d262de37fc73ba1db7d4
Taekyue Park <tapark@c3r7s7.42seoul.kr>
2021-03-05 05:21:34 +0900

    ing

---
+++ union/union.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+	int k;
+
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	i = 0;
+	while (argv[1][i] != '\0')
+	{
+		k = 0;
+		while (argv[1][i] != argv[1][k])
+			k++;
+		if (i == k)
+			write(1, &argv[1][i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
