64db7ffc7318345aa5fe17713da022f9566b0e05
tapark <tg911022@naver.com>
2021-03-06 03:40:42 +0900

    gnl

--- union/union.c
+++ union/union.c
@@ -16,12 +16,30 @@ int main(int argc, char **argv)
 	while (argv[1][i] != '\0')
 	{
 		k = 0;
-		while (argv[1][i] != argv[1][k])
+		while (argv[1][i] != argv[1][k] && argv[1][k] != '\0')
 			k++;
 		if (i == k)
 			write(1, &argv[1][i], 1);
 		i++;
 	}
+
+	i = 0;
+	while (argv[2][i] != '\0')
+	{
+		j = 0;
+		while (argv[2][i] != argv[1][j] && argv[1][j] != '\0')
+			j++;
+		if (argv[1][j] == '\0')
+		{
+			k = 0;
+			while (argv[2][i] != argv[2][k] && argv[2][k] != '\0')
+				k++;
+			if (i == k)
+				write(1, &argv[2][i], 1);
+		}
+		i++;
+	}
+
 	write(1, "\n", 1);
 	return (0);
 }
